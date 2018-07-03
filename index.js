83fe6e981df5afcafc176e4d62551f512f630bad
Alyssa Vongries <alyssavongries@gmail.com>
2018-07-03 02:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ var a = Math.floor(Math.random() * 1000)
 var b = Math.floor(Math.random() * 1000)
 
 function add(a,b){
-  return a + b
+  return a + b;
 }
 
 function subtract(a,b){
-  return a - b
+  return a - b;
 }
 
 function multiply(a,b){
-  return a * b
+  return a * b;
 }
 
-function divide
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n){
+  return parseInt(n,10);
+}
+
+function dec(n){
+  return parseFloat(n);
+}
+
+
+}
